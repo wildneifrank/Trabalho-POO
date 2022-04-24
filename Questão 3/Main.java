761b7453a6a1d9b0e24aeea471d01cfacf3d782b
Fábio Gabriel <fabiogabrieleig@gmail.com>
2022-04-24 16:19:43 -0300

    Conjuntos

---
+++ Questão 3/Main.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+        int[] naturais = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] pares = {0, 2, 4, 6, 8};
+        int[] impares = {1, 3, 5, 7, 9};
+        int[] primos = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
+
+        System.out.println(impares[2]);
+    }
+}
