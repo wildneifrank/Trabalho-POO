3a706b06df328007d73733f127a18be7bbbb8a7a
Fábio Gabriel <fabiogabrieleig@gmail.com>
2022-04-24 17:19:44 -0300

    Item A

--- Questão 3/Main.java
+++ Questão 3/Main.java
@@ -1,3 +1,6 @@
+import java.io.*;
+import java.util.*;
+
 class Main {
     public static void main(String[] args) {
         int[] naturais = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
@@ -5,6 +8,14 @@ class Main {
         int[] impares = {1, 3, 5, 7, 9};
         int[] primos = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
 
-        System.out.println(impares[2]);
+        Set<Integer> prim = new HashSet<Integer>();
+        Set<Integer> impar = new HashSet<Integer>();
+        Set<Integer> par = new HashSet<Integer>();
+        Set<Integer> nat = new HashSet<Integer>();
+
+        System.out.println("Item A -> 4 é subconjunto de si mesmo :" + prim.containsAll(prim));
+        System.out.println("Item B -> Pertinência do conjunto 2 e 3 :" + prim.containsAll(prim));
+        System.out.println("Item A -> 4 é subconjunto de si mesmo :" + prim.containsAll(prim));
+        System.out.println("Item A -> 4 é subconjunto de si mesmo :" + prim.containsAll(prim));
     }
 }
